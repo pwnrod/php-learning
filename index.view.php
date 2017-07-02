043748947e3c8c5ef68d8328e96051db267f5735
Caylin James <caylinjames@Caylins-MacBook-Pro.local>
2017-07-02 13:41:00 -0400

    Episode 5: Separate PHP Logic From Presentation

---
+++ index.view.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <title>Document</title>
+    <style>
+        header {
+            background: #e3e3e3;
+            padding: 2em;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+<header>
+    <h1>
+        <?= $greeting; ?>
+    </h1>
+</header>
+
+</body>
+</html>
