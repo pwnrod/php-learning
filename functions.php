9a5ecdc591c483df616912bbf611a65edda9d705
Caylin James <caylinjames@Caylins-MacBook-Pro.local>
2017-07-02 23:09:44 -0400

    Intro to PDO

--- functions.php
+++ functions.php
@@ -1,15 +1,28 @@
 <?php
 
+function connectToDb() {
+    // Try to connect to database, if an error occurs die and show error message
+    try {
+        return new PDO('mysql:host=127.0.0.1;dbname=mytodo', 'root', '');
+    } catch (PDOException $e) {
+        die($e->getMessage());
+    }
+}
+
+function fetchAllTasks($pdo)
+{
+    // First, prepare your statement
+    $statement = $pdo->prepare('select * from todos');
+
+    // Next, execute your statement
+    $statement->execute();
+
+    // Lastly, fetch rows from the results
+    return $statement->fetchAll(PDO::FETCH_CLASS, 'Task');
+}
+
 function dd($data) {
     echo '<pre>';
     die(var_dump($data));
     echo '</pre>';
-}
-
-function checkAge($age) {
-    if ($age >= 21) {
-        return true;
-    } else {
-        return false;
-    }
 }
--- index.php
+++ index.php
@@ -1,31 +1,9 @@
 <?php
 
-class Task {
-    public $description;
-    public $completed = false;
+require 'functions.php';
+require 'Task.php';
 
-    public function __construct($description)
-    {
-        $this->description = $description;
-    }
-
-    public function complete()
-    {
-        $this->completed = true;
-    }
-
-    public function isComplete()
-    {
-        return $this->completed;
-    }
-}
-
-$tasks = [
-    new Task('Go to the store'),
-    new Task('Finish my screencast'),
-    new Task('Clean my room')
-];
-
-$tasks[0]->complete();
+$pdo = connectToDb();
+$tasks = fetchAllTasks($pdo);
 
 require 'index.view.php';
